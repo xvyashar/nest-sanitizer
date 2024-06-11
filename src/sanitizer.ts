8a55a0b1c40b2cd7d0dacb69b051daab38de6a97
XV Yashar <xvyashar@gmail.com>
2024-06-11 03:44:32 +0330

    ref: fix npm entrypoint issue

--- src/sanitizer.ts
+++ src/sanitizer.ts
@@ -1,6 +1,6 @@
 import { UsePipes, applyDecorators } from '@nestjs/common';
 import { SanitizerPipe } from './sanitizer.pipe';
 
-export function UseSanitizer(options: any) {
+export function UseSanitizer(options?: any) {
   return applyDecorators(UsePipes(new SanitizerPipe(options)));
 }
